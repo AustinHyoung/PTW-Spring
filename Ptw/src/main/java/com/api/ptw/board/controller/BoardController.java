d682d0a068db56e1064e1d208f5b6a8ad66df08d
Hakyoung <jhy2297@naver.com>
2023-04-10 19:08:52 +0900

    update after data

--- Ptw/src/main/java/com/api/ptw/board/controller/BoardController.java
+++ Ptw/src/main/java/com/api/ptw/board/controller/BoardController.java
@@ -278,7 +278,9 @@ public class BoardController {
 			
 			boardService.setCount(paramMap);
 			
-			resObj.put("msg", "업데이트 완료");
+			
+			
+			System.out.println(resObj);
 			
 			return resObj;
 		} catch(Exception e) {
@@ -298,6 +300,43 @@ public class BoardController {
 			
 			boardService.setCardsListPosition(paramMap);
 			
+			resObj.put("board_no", paramMap.get("board_no"));
+			resObj.put("title", paramMap.get("title"));
+			
+			// db에서 가져온 데이터 cards_list
+			List<Map<String, Object>> cards_list = boardService.getCardList(paramMap);
+			
+			// db에서 가져온 데이터 card
+			List<Map<String, Object>> card = boardService.getCard(paramMap);
+			
+			// resObj에 cards_list 로 가져올 리스트
+			List<Map<String, Object>> cardsList = new ArrayList<Map<String, Object>>();
+			
+			// 가져온 cards_list 데이터를 반복시킴
+			for(Map<String, Object> cardListMap: cards_list) {
+				// map을 만들어서 하나씩 데이터를 새로운 key로 db에서 가져온 데이터를 삽입
+				Map<String, Object> cardsListMap = new HashMap<String, Object>();
+				cardsListMap.put("cards_list_no", cardListMap.get("cards_list_no"));
+				cardsListMap.put("title", cardListMap.get("title"));
+				cardsListMap.put("list_order", cardListMap.get("list_order"));
+				// 새로운 card의 리스트 생성 
+				List<Map<String, Object>> cardsListCardList = new ArrayList<Map<String, Object>>();
+				// 가져온 card 데이터를 반복시킴
+				for (Map<String, Object> cardMap: card) {
+					// card와 card_list의 no이 같은것만 map에 삽입
+					if(cardMap.get("card_list_no").equals(cardListMap.get("cards_list_no"))) {
+						Map<String, Object> cardsListCardMap = new HashMap<String, Object>();
+						cardsListCardMap.put("card_no", cardMap.get("card_no"));
+						cardsListCardMap.put("contents", cardMap.get("contents"));
+						cardsListCardList.add(cardsListCardMap);
+					}
+				}
+				cardsListMap.put("card", cardsListCardList);
+				cardsList.add(cardsListMap);
+			}
+			
+			
+			resObj.put("cards_list", cardsList);
 			
 			resObj.put("msg", "업데이트 완료");
 			
